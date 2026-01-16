bb31a8a652dd2f3398554484834eb47cff27a72b
Erik Ferreira <erik.souza@vanguardatech.com>
2026-01-15 23:31:31 -0300

    correction env to create user

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+import z from "zod"
 import fastify from "fastify"
 
+import { appRoutes } from "./http/routes"
+
+import { prisma } from "./lib/prisma"
+
 export const app = fastify()
+
+app.register(appRoutes)
