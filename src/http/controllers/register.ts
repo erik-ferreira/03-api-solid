f4462b0e85aa011f8c36c077c2144408aa0081b8
Erik Ferreira <erik.souza@vanguardatech.com>
2026-01-20 23:10:08 -0300

    create instance error user already exists

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,6 +4,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { RegisterUseCase } from "@/use-cases/register"
 
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -20,7 +21,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send()
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
