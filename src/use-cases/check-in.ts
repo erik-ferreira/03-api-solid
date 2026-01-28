d3f23508aa66376f5bafd656ea39dccfefeb6e4b
erik-ferreira <erikdesouzagf@gmail.com>
2026-01-28 00:02:56 -0300

    start user case check in

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,31 @@
+import { hash } from "bcryptjs"
+
+import { CheckIn } from "generated/prisma/browser"
+import { UsersRepository } from "@/repositories/users-repository"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
+import { CheckInsRepository } from "@/repositories/check-ins-repository"
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return { checkIn }
+  }
+}
