c2cad94537366edd3a7985df652014aa488fe192
erik-ferreira <erikdesouzagf@gmail.com>
2026-01-30 00:57:26 -0300

    start create gym repository

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,13 +1,13 @@
-import { hash } from "bcryptjs"
-
 import { CheckIn } from "generated/prisma/browser"
-import { UsersRepository } from "@/repositories/users-repository"
-import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 import { CheckInsRepository } from "@/repositories/check-ins-repository"
+import { GymsRepository } from "@/repositories/gyms-repository"
+import { ResourceNotFoundError } from "./errors/resources-not-found-error"
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -15,12 +15,23 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    // calculate distance between user and gym
+
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
