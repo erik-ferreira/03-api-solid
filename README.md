f4a9cf4401120810b281a0a6c09d5ea97d695c3d
erik-ferreira <erikdesouzagf@gmail.com>
2026-02-06 01:29:24 -0300

    add new test

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Funcionalidades da aplicação, ou seja, o que vai ser possível do usuário faz
 - [x] Deve ser possível obter o perfil de um usuário logado;
 - [x] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
 - [x] Deve ser possível o usuário ver seu histórico de check-ins;
-- [ ] Deve ser possível o usuário buscar academias próximas (até 10km);
+- [x] Deve ser possível o usuário buscar academias próximas (até 10km);
 - [x] Deve ser possível o usuário buscar academias pelo nome;
 - [x] Deve ser possível o usuário realizar check-in em uma academia;
-- [ ] Deve ser possível validar o check-in de um usuário;
+- [x] Deve ser possível validar o check-in de um usuário;
 - [x] Deve ser possível cadastrar uma academia;
 
 ## RNs(Regras de negócio)
--- src/use-cases/validate-check-ins.spec.ts
+++ src/use-cases/validate-check-ins.spec.ts
@@ -3,6 +3,8 @@ import { beforeEach, describe, expect, it, afterEach, vi } from "vitest"
 import { ValidateCheckInUseCase } from "./validate-check-in"
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository"
 
+import { ResourceNotFoundError } from "./errors/resources-not-found-error"
+
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
 
@@ -30,5 +32,13 @@ describe("Validate Check Ins Use Case", () => {
 
     expect(checkIn.validated_at).toEqual(expect.any(Date))
     expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it("should be able to validate an inexistent check in", async () => {
+    await expect(() =>
+      sut.execute({
+        checkInId: "inexistent-check-in-id",
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
