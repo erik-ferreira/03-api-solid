a7526e1d19c12eef9189e6af1b9c9d9abbfe5d47
erik-ferreira <erikdesouzagf@gmail.com>
2026-02-07 23:41:12 -0300

    implement prisma gyms repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,43 @@
+import { Gym } from "generated/prisma/browser"
+import { GymCreateInput } from "generated/prisma/models"
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository"
+import { prisma } from "@/lib/prisma"
+
+export class PrismaGymRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: { id },
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+       WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async create(data: GymCreateInput) {
+    const gym = await prisma.gym.create({ data })
+
+    return gym
+  }
+}
