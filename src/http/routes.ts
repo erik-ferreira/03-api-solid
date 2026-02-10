729845f946724b714ea724512608a5d482fffbca
erik-ferreira <erikdesouzagf@gmail.com>
2026-02-09 23:49:08 -0300

    start config to use jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from "fastify"
 
+import { profile } from "@/http/controllers/profile"
 import { register } from "@/http/controllers/register"
 import { authenticate } from "@/http/controllers/authenticate"
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register)
-
   app.post("/session", authenticate)
+
+  // Authenticated
+  app.get("/me", profile)
 }
