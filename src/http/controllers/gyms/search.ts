a43b98c95f7bad2c1536ebc8505d85e0d7a67afb
erik-ferreira <erikdesouzagf@gmail.com>
2026-02-11 17:32:40 -0300

    create controllers gyms

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import z from "zod"
+import { FastifyReply, FastifyRequest } from "fastify"
+
+import { makeSearchGymsUseCaseUseCase } from "@/use-cases/factories/make-search-gyms-use-case"
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { q, page } = searchGymQuerySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCaseUseCase()
+
+  const gyms = await searchGymUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(200).send({ gyms })
+}
