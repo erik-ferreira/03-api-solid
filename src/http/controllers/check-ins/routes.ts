59f68c87c1e942641d02f03f2ee6ecf9841ce149
erik-ferreira <erikdesouzagf@gmail.com>
2026-02-16 18:03:50 -0300

    fix in util and tests to create user with role

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -6,6 +6,7 @@ import { metrics } from "./metrics"
 import { validate } from "./validate"
 
 import { verifyJWT } from "../../middlewares/verify-jwt"
+import { verifyUserRole } from "@/http/middlewares/verify-user-role"
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT)
@@ -15,5 +16,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
 
   app.post("/gyms/:gymId/check-ins", create)
 
-  app.patch("/check-ins/:checkInId/validate", validate)
+  app.patch(
+    "/check-ins/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    validate,
+  )
 }
